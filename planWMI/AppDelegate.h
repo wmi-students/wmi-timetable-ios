99077b39cf27e96238b9953a6f4ba4cbdf34891a
wojek <wojek@wojeks-macbook-pro.local>
2016-10-03 12:37:13 -0700

    Initial Commit

---
+++ planWMI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  planWMI
+//
+//  Created by wojek on 03/10/16.
+//  Copyright © 2016 wojek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
